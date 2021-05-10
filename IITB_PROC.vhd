1a39ab665649326a325faf84c5ebe95604820527
bhanu <70092039+bhanuprakash789@users.noreply.github.com>
2021-05-10 11:28:40 +0530

    Add files via upload

---
+++ IITB_PROC.vhd
@@ -0,0 +1,6 @@
+library ieee, std;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_textio.all;
+use ieee.numeric_std.all;
+use std.textio.all;
+
---
+++ testbench.vhd
@@ -0,0 +1,7 @@
+library ieee, std;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_textio.all;
+use std.textio.all;
+
+entity testbench is
+end entity;
